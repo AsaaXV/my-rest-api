4a9c3573e67f722eeefb956b18c1ed881373a224
AsaaXV <89570595+AsaaXV@users.noreply.github.com>
2023-08-07 16:55:51 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.MONGO_DB_URI = "mongodb+srv://nazokyoukai:1g2g3g4g5g@cluster0.cecqnhn.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "memek" //isi apa aja bebas
+global.your_email = "xmlpreset124@gmail.com" //email
+global.email_password = "xgqfbhatdqyvmsij" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
